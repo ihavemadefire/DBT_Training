36793e848732398cef131accddb77ec006702e07
Jacob Ide <jacobidedata@gmail.com>
2022-09-22 00:02:20 +0000

    developed reporting

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -8,11 +8,13 @@ o.ordersellingprice - o.ordercostprice as orderprofit,
 o.ordercostprice,
 o.ordersellingprice,
 --raw customer
+c.customerid,
 c.customername,
 c.segment,
 c.country,
 -- Raw product
 p.category,
+p.productid,
 p.productname,
 p.subcategory
 from {{ ref('raw_orders') }} as o
